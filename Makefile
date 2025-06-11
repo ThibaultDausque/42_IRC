aa7686d36f20badf88af890d36a015ef782c33a8
Tristan Pipi <tpipi@2E5.42angouleme.fr>
2025-06-11 19:05:17 +0200

    Command class

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = c++
 FLAGS = -Wall -Wextra -Werror -std=c++98
-SRCS = main.cpp \
+SRCS = srcs/main.cpp srcs/Command.cpp
 RM = rm -f
 OBJS_DIR = obj
 OBJS = $(SRCS:%.cpp=$(OBJS_DIR)/%.o)
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,11 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpipi <tpipi@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 16:23:42 by tpipi             #+#    #+#             */
+/*   Updated: 2025/06/11 19:04:06 by tpipi            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+#include "Command.hpp"
 
 int	main(int ac, char **av)
 {
+	(void)av;
+	std::string line;
+
 	if (ac != 3)
 	{
-		std::cout << "* three arguments needed *" << std::endl;
+		std::cout << "* two arguments needed *" << std::endl;
 		return 0;
 	}
+	std::getline(std::cin, line);
+
+	std::string cmd = line.substr(0, line.find(" "));
+	// separer les \n en plusieurs lignes (vector<std::string> ???)
+	// transformer les whitespaces en espace normal (verifie si c'est Hexchat ou pas)
+	// chercher si les messages de base doivent etre parse d'une certaine maniere
+	if (isCmdValid(cmd))
+		std::cout << ">> La commande " << cmd << " est bien valide !" << std::endl;
+	else
+		std::cout << ">> La commande " << cmd << " n'est pas valide !" << std::endl;
 	return 0;
 }
