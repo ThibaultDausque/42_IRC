4e5561a05be4c82563021201fa9655a95bde7978
Tristan Pipi <tpipi@1D4.42angouleme.fr>
2025-07-04 05:49:20 +0200

    Reorganisation des ERR_NUM par ordre croissant de haut en bas

--- includes/NumericReply.hpp
+++ includes/NumericReply.hpp
@@ -6,7 +6,7 @@
 /*   By: tpipi <tpipi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 15:24:32 by tpipi             #+#    #+#             */
-/*   Updated: 2025/07/04 05:20:30 by tpipi            ###   ########.fr       */
+/*   Updated: 2025/07/04 05:48:53 by tpipi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,23 @@
 # define DEFAULT_REASON ":No reason given\r\n"
 
 # define ERR_NOSUCHNICK(nick, nonexistant) ":"SERVER_HOSTNAME" 401 "+nick+" "+nonexistant+" :No such nick/channel\r\n" 
-# define ERR_NEEDMOREPARAM(nick, command) ":"SERVER_HOSTNAME" 461 "+nick+" "+command+" :Not enough parameters\r\n"
-# define ERR_ALREADYREGISTRED(nick) ":"SERVER_HOSTNAME" 462 "+nick+" :You may not reregister\r\n"
 # define ERR_NOSUCHCHANNEL(nick, channel) ":"SERVER_HOSTNAME" 403 "+nick+" "+channel+" :No such channel\r\n"
-# define ERR_BADCHANNELKEY(nick, channel) ":"SERVER_HOSTNAME" 475 "+nick+" "+channel+" :Cannot join channel (+k)\r\n"
-# define ERR_CHANNELISFULL(nick, channel) ":"SERVER_HOSTNAME" 471 "+nick+" "+channel+" :Cannot join channel (+l)\r\n"
-# define ERR_INVITEONLYCHAN(nick, channel) ":"SERVER_HOSTNAME" 473 "+nick+" "+channel+" :Cannot join channel (+i)\r\n"
+# define ERR_NORECIPIENT(nick, command) ":"SERVER_HOSTNAME" 411 "+nick+" :No recipient given ("+command+")\r\n"
+# define ERR_NOTEXTTOSEND(nick) ":"SERVER_HOSTNAME" 412 "+nick+" :No text to send\r\n"
+# define ERR_UNKNOWNCOMMAND(nick, command) ":"SERVER_HOSTNAME" 421 "+nick+" "+command+" :Unknown command\r\n"
 # define ERR_NONICKNAMEGIVEN(nick) ":"SERVER_HOSTNAME" 431 "+nick+" :No nickname given\r\n"
 # define ERR_ERRONEUSNICKNAME(nick, newnick) ":"SERVER_HOSTNAME" 432 "+nick+" "+newnick+" :Erroneus nickname\r\n"
 # define ERR_NICKNAMEINUSE(nick, newnick) ":"SERVER_HOSTNAME" 433 "+nick+" "+newnick+" :Nickname is already in use\r\n"
-# define ERR_NOTREGISTERED(nick) ":"SERVER_HOSTNAME" 451 "+nick+" :You have not registered\r\n"
-# define ERR_CHANOPRIVSNEEDED(nick, channel) ":"SERVER_HOSTNAME" 482 "+nick+" "+channel+" :You're not channel operator\r\n"
 # define ERR_USERNOTINCHANNEL(originNick, targetNick, channel) ":"SERVER_HOSTNAME" 441 "+originNick+" "+targetNick+" "+channel+" :They aren't on that channel\r\n"
 # define ERR_NOTONCHANNEL(nick, channel) ":"SERVER_HOSTNAME" 442 "+nick+" "+channel+" :You're not on that channel\r\n"
-# define ERR_NORECIPIENT(nick, command) ":"SERVER_HOSTNAME" 411 "+nick+" :No recipient given ("+command+")\r\n"
-# define ERR_NOTEXTTOSEND(nick) ":"SERVER_HOSTNAME" 412 "+nick+" :No text to send\r\n"
 # define ERR_USERONCHANNEL(nick, invited, channel) ":"SERVER_HOSTNAME" 443 "+nick+" "+invited+" "+channel+" :is already on channel\r\n" 
-# define ERR_UNKNOWNCOMMAND(nick, command) ":"SERVER_HOSTNAME" 421 "+nick+" "+command+" :Unknown command\r\n"
+# define ERR_NOTREGISTERED(nick) ":"SERVER_HOSTNAME" 451 "+nick+" :You have not registered\r\n"
+# define ERR_NEEDMOREPARAM(nick, command) ":"SERVER_HOSTNAME" 461 "+nick+" "+command+" :Not enough parameters\r\n"
+# define ERR_ALREADYREGISTRED(nick) ":"SERVER_HOSTNAME" 462 "+nick+" :You may not reregister\r\n"
+# define ERR_CHANNELISFULL(nick, channel) ":"SERVER_HOSTNAME" 471 "+nick+" "+channel+" :Cannot join channel (+l)\r\n"
+# define ERR_INVITEONLYCHAN(nick, channel) ":"SERVER_HOSTNAME" 473 "+nick+" "+channel+" :Cannot join channel (+i)\r\n"
+# define ERR_BADCHANNELKEY(nick, channel) ":"SERVER_HOSTNAME" 475 "+nick+" "+channel+" :Cannot join channel (+k)\r\n"
+# define ERR_CHANOPRIVSNEEDED(nick, channel) ":"SERVER_HOSTNAME" 482 "+nick+" "+channel+" :You're not channel operator\r\n"
 
 # define RPL_WELCOME(nick) ":"SERVER_HOSTNAME" 001 "+nick+" :Welcome to the 42Angouleme IRC Network, "+nick+"\r\n"
 # define RPL_YOURHOST(nick) ":"SERVER_HOSTNAME" 002 "+nick+" :Your host is irc.42angouleme.fr, running version 4.2\r\n"
